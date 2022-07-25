d0188ac4b8fb6389069f930e1b43dcf63e18294c
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2022-07-25 22:45:51 +0530

    Time: 10 ms (43.81%), Space: 11.8 MB (15.50%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,13 +1,69 @@
 class Solution {
+    private:
+    int Dbs(vector<int>& nums, int target){
+        int e = nums.size()-1;
+        int s = 0 ;
+        int index = -1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            
+            if(nums[mid] == target){
+                index = mid;
+                
+            }
+          if(nums[mid]<=target){
+                s = mid+1;
+            }
+            else{
+             e = mid-1;
+            }
+            
+            
+            
+        }
+                   // cout<<index<<" "<<endl;
+        return index;
+    }
+       int Fbs(vector<int>& nums, int target){
+        int e = nums.size()-1;
+        int s = 0 ;
+        int index = -1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            
+            if(nums[mid] == target){
+                index = mid;
+                
+            }
+            if(nums[mid]>=target){
+                e = mid -1;
+            }
+            else{
+               s = mid+1;
+            }
+            
+            
+            
+        }
+           // cout<<index<<" "<<endl;
+           return index;
+    }
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-      sort(nums.begin(),nums.end());
-        vector<int>ans;
-        for(int i = 0 ;i<nums.size();i++ ){
-            if(nums[i] == target){
-                ans.push_back(i);
-            }
+        sort(nums.begin(),nums.end());
+        
+        int first_occurance = Fbs(nums,target);
+                    int second_occurance = Dbs(nums,target);
+            
+            vector<int>ans;
+        if(first_occurance == -1 || second_occurance == -1){
+            return ans;
         }
+        for(int i = first_occurance ; i <=second_occurance;i++ ){
+            ans.push_back(i);
+        }
+        
         return ans;
+
     }
 };
