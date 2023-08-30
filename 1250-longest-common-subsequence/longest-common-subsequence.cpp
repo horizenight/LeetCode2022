0f418d32e7ac99366b7b15a1e5005c34fd670efe
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-08-30 18:06:50 +0530

    Time: 51 ms (67.38%) | Memory: 24.6 MB (29.10%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -35,11 +35,32 @@ public:
         
     }
 
+    int solveUsingTab(string a , string b){
+        vector<vector<int>> dp(a.size() + 1 , vector<int> (b.size()+1 , 0));
+
+        for(int i = a.length() - 1 ; i>= 0 ;i--){
+            for(int j = b.length() - 1 ; j >= 0 ; j--){
+                int ans =0;
+                if(a[i] == b[j]){
+                    ans  = 1 + dp[i+1][j+1];
+                }
+                else{
+                    ans = 0 + max(dp[i+1][j] ,dp[i][j+1]);
+                }
+                dp[i][j] = ans;
+            }
+        } 
+
+    return dp[0][0];
+
+        
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         int i = 0;
         int j = 0;
-        vector<vector<int>> dp(text1.size() + 1 , vector<int> (text2.size()+1 , -1));
-        int ans = solveUsingMem(text1, text2, 0, 0,dp);
+        // vector<vector<int>> dp(text1.size() + 1 , vector<int> (text2.size()+1 , -1));
+        int ans = solveUsingTab(text1, text2);
 
         return ans;
     }
