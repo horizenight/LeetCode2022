154430a7a19f075665fbb3b68867968250733c6c
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-09-01 00:06:12 +0530

    Time: 2 ms (73.50%) | Memory: 6.1 MB (26.09%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int recSolve(int n){
-        if(n == 0 || n == 1){
+    int suR(int n){
+        if(n <= 1){
             return n;
         }
 
-        int ans = recSolve(n-1) + recSolve(n-2);
+        int ans = suR(n-1) + suR(n-2);
         return ans;
     }
+
+    int suRM(int n , vector<int> &dp){
+        if(n <= 1){
+                    return n;
+             }
+             if(dp[n] != -1){
+                 return dp[n];
+             }
+
+                int ans = suRM(n-1,dp) + suRM(n-2,dp);
+                return dp[n]  = ans;
+    }
+    
     int fib(int n) {
-        return recSolve(n);
+        vector<int> dp(n+1,-1);
+        int ans = suRM( n,dp);
+        return ans;
     }
 };
