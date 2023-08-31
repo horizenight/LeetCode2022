9a490970e528dd287a8eede81ed1a90b321c0255
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-08-31 23:53:19 +0530

    Time: 3 ms (21.30%) | Memory: 6.2 MB (41.42%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
+    int solveUsingRecursion(int n){
+        if(n == 0 || n == 1 ){
+            return 1;
+        }
+       
+        int ans = solveUsingRecursion(n-1) + solveUsingRecursion(n-2);
+
+        return ans;
+    }
+    int solveUsingMem(int n , vector<int> &dp){
+           if(n == 0 || n == 1 ){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+       
+        int ans = solveUsingMem(n-1,dp) + solveUsingMem(n-2,dp);
+
+        return dp[n] = ans;
+    }
+
     int climbStairs(int n) {
+        // insitalising dp array 
         vector<int> dp(n+1,-1);
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for(int i = 2; i <= n; i++)
-            dp[i] = dp[i-1] + dp[i-2];
-
-        return dp[n]; // Moved this line inside the function
+        int ans = solveUsingMem( n ,dp);
+        return ans;
     }
 };
