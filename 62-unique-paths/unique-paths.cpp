64477721de678751ab88e0e19759643c5a3a0a13
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-09-03 19:27:53 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (6.60%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -29,9 +29,21 @@ public:
 
         return dp[m][n] = count;
     }
+
+    int soT(int m ,int n){
+        vector<vector<int>>dp(m+2,vector<int>(n+2,1));
+      
+        for(int i = 1; i<m ; i++){
+            for(int j = 1; j<n;j++){
+                dp[i][j]  = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        int ans = soM(m,n,dp);
+        int ans = soT(m,n);
         return ans;
     }
 };
