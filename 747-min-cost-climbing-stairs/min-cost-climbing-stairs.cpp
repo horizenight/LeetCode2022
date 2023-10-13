e479701c62a42f69cb564998f35387836d318fd3
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-10-13 10:48:12 +0530

    Time: 3 ms (84.96%) | Memory: 14.2 MB (16.21%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    int soR(vector<int>& cost, int n){
-        if(n == 0 || n == 1) {
-            return 0;
+    int soR(vector<int>&cost,int n){
+        if(n == 0 || n ==1){
+            return 0 ;
         }
+        int costi = min(cost[n-1] + soR(cost,n-1) , cost[n-2] + soR(cost,n-2));
 
-        int ans = min(cost[n-1] + soR(cost,n-1) , cost[n-2]+ soR(cost,n-2));
-
-
-        return ans;
-
-
+        return costi;
     }
-    int soM(vector<int> & cost , int n , vector<int>&dp){
-         if(n == 0 || n == 1) {
-            return 0;
+    int soM(vector<int>&cost, int n , vector<int>&dp){
+        if(n == 0 || n ==1){
+            return 0 ;
         }
         if(dp[n] != -1){
             return dp[n];
         }
-        int ans = min(cost[n-1] + soM(cost,n-1,dp) , cost[n-2]+ soM(cost,n-2,dp));
+        int costi = min(cost[n-1] + soM(cost,n-1,dp) , cost[n-2] + soM(cost,n-2,dp));
 
-        return dp[n] = ans;
-
+        return dp[n] = costi;
     }
-
-    int soT(vector<int> & cost , int n){
-        vector<int>dp(n+1,0);
-        //base case already handled
-        for(int i = 2 ; i <= n ;i++){
-            dp[i] = min(cost[i-1] + dp[i-1] , cost[i-2] + dp[i-2]);
-        }
-        return dp[n];
-    }
-    
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size() ;
-        vector<int>dp(n+1,-1);
-        int ans = soT(cost ,n);
-        return ans;
+      int n = cost.size();
+      vector<int>dp(n+1, -1);
+      int ans = soM(cost,n,dp);
+      return ans;  
     }
 };
