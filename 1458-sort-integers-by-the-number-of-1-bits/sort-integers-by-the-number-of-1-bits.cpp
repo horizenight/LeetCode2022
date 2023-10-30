9a9c9672be53ac6b78d8392d3a6e1b5bf35f9d40
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-10-30 14:14:01 +0530

    Time: 14 ms (17.40%) | Memory: 10.4 MB (66.29%) - LeetSync

---
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int find1BitCount(int num){
+        int count = 0;
+        while(num!= 0){
+            if(num&1){
+                count++;
+            }
+            num>>=1;
+        }
+
+        return count;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        // sort function using lambda comperator 
+        // auto lambda = [ &a ](int a , int b) { body }
+        // [ ] // capture clause BY reference extra copy wouldnt be taken
+        // ( ) // receiving parameters 
+        // int countOneA = find1BitsCount(a);
+        // int countOneB = findbitsCount(b); 
+        // if(cA == cB) return a < b;
+        // return cA<CB;
+
+        auto lambda = [&](int &a , int &b){
+            int count_a = find1BitCount(a);
+            int count_b = find1BitCount(b);
+
+            if(count_a == count_b){
+                return a<b;
+            }
+
+            return count_a<count_b;
+
+        };
+
+        sort(begin(arr),end(arr),lambda);
+        return arr;
+
+    }
+};
