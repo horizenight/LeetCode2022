9635c02b6dc35d7928e79e98b998189efe8fe236
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-10-30 14:14:33 +0530

    Time: 4 ms (84.43%) | Memory: 10.6 MB (53.39%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -22,8 +22,9 @@ public:
         // return cA<CB;
 
         auto lambda = [&](int &a , int &b){
-            int count_a = find1BitCount(a);
-            int count_b = find1BitCount(b);
+            // int count_a = find1BitCount(a);
+            int count_a = __builtin_popcount(a);
+            int count_b = __builtin_popcount(b);
 
             if(count_a == count_b){
                 return a<b;
