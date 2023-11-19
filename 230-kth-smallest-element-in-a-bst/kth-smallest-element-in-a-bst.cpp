4b9e8982bc265e84fdd8fe055bf575b886e1ef4c
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-11-19 20:14:54 +0530

    Time: 14 ms (54.55%) | Memory: 24.3 MB (97.38%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,30 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// class Solution {
+// public:
+//     void inorder(TreeNode * root , vector<int>&ans){
+//         if(!root) return ;
+//         // LNR => inorder 
+//         inorder(root->left,ans);
+//         ans.push_back(root->val);
+//         inorder(root->right,ans);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int>ans;
+//         inorder(root,ans);
+//         for(auto it : ans){
+//             cout<<it<<" ";
+//         }
+//         return ans[k-1];
+//     }
+// };
+
 class Solution {
 public:
-    void inorder(TreeNode * root , vector<int>&ans){
-        if(!root) return ;
-        // LNR => inorder 
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int>ans;
-        inorder(root,ans);
-        for(auto it : ans){
-            cout<<it<<" ";
+    int kthSmallest(TreeNode* root, int&k) {
+        
+        // base case 
+        if(root==NULL) return -1;
+// inorder 
+        // left
+        int leftAns = kthSmallest(root->left,k);
+        if(leftAns != -1){
+            return leftAns;
+        }        // node 
+        k--;
+        if(k==0){
+            return root->val;
         }
-      
-        return ans[k-1];
+        int rightAns = kthSmallest(root->right,k);
+        return rightAns;
+        
     }
 };
