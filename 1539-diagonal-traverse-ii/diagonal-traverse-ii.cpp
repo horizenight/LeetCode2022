d57433cd965d3a2c4589cccbe0a8fe6ee5f24293
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-11-22 17:25:52 +0530

    Time: 113 ms (93.59%) | Memory: 66.1 MB (90.60%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        // Diagonal Traversal => Think about Map always 
-        // Diagonal Element Row + col value is constant 
+        queue<pair<int,int>> que;
+        que.push({0,0});
+        vector<int> result ; 
+        while(!que.empty()){
+            auto [row,col] = que.front();
+            que.pop();
 
-        // Map [Row+col] = Values<int> 
-        unordered_map<int,vector<int>> mp;
+            result.push_back(nums[row][col]);
+            if(col == 0 && row +1 < nums.size())
+                 que.push({row+1,col}); 
+            if(col+1 < nums[row].size())  
+                que.push({row,col+1});  
 
-        for(int row = nums.size()-1 ;row>=0 ; row-- ){
-            for(int col = 0 ; col<nums[row].size() ; col++ ){
-                int diagonal = row+col; 
-                mp[diagonal].push_back(nums[row][col]);
-            }
-        }
-
-        vector<int>result;
-        int diagonal = 0 ;
-        while(mp.find(diagonal) != mp.end() ){
-            for(int &num : mp[diagonal]){
-                result.push_back(num);
-            }
-            diagonal++;
         }
 
         return result;
     }
 };
+
+
+// Using Map Approach 
+ 
+// class Solution {
+// public:
+//     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+//         // Diagonal Traversal => Think about Map always 
+//         // Diagonal Element Row + col value is constant 
+
+//         // Map [Row+col] = Values<int> 
+//         unordered_map<int,vector<int>> mp;
+
+//         for(int row = nums.size()-1 ;row>=0 ; row-- ){
+//             for(int col = 0 ; col<nums[row].size() ; col++ ){
+//                 int diagonal = row+col; 
+//                 mp[diagonal].push_back(nums[row][col]);
+//             }
+//         }
+
+//         vector<int>result;
+//         int diagonal = 0 ;
+//         while(mp.find(diagonal) != mp.end() ){
+//             for(int &num : mp[diagonal]){
+//                 result.push_back(num);
+//             }
+//             diagonal++;
+//         }
+//         return result;
+//     }
+// };
