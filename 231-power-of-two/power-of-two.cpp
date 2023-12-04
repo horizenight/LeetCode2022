6d9d1b4d6a92c8218b914dbfff0275144098e158
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-12-05 03:04:29 +0530

    Time: 7 ms (16.34%) | Memory: 6.2 MB (78.54%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         int ans = 1; 
+        if(n == 1 ) return true;
         for(int i = 0 ; i <= 30 ;i++){
-             
-            if(ans == n) return true;
-            if(ans < INT_MAX /2 ){
+            
+             if(ans < INT_MAX /2 ){
                 ans = ans * 2;
                 cout<<ans<<endl;
             }
+            if(ans == n) return true;
            
         }
 
