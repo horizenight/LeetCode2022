a1c31fc1447636444657c005df04ae076da4df31
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-12-05 11:38:19 +0530

    Time: 2 ms (44.05%) | Memory: 6.4 MB (36.15%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
-
     int numberOfMatches(int n) {
-        if(n== 1) return 0;
-        if(n&1){
-            // n is odd 
-            return  ( (n - 1) / 2 )  + numberOfMatches((n - 1) / 2 + 1);
-        }else{
-            return (n/2) + numberOfMatches(n/2);
+        int match  = 0;
+        while(n > 1){
+            if(!(n&1)){
+                // n is even 
+               int matchcount=n/2;
+               n=n/2;  
+               match+=matchcount;
+            }else{
+                int matchcount=(n-1)/2;
+                 n=matchcount+1; 
+                 match+=matchcount;  
+            }
         }
+
+
+        return match;
     }
 };
+
+// Recursive 
+// class Solution {
+// public:
+
+//     int numberOfMatches(int n) {
+//         if(n== 1) return 0;
+//         if(n&1){
+//             // n is odd 
+//             return  ( (n - 1) / 2 )  + numberOfMatches((n - 1) / 2 + 1);
+//         }else{
+//             return (n/2) + numberOfMatches(n/2);
+//         }
+//     }
+// };
+
