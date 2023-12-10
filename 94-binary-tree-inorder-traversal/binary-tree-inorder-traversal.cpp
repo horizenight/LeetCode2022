875f78000745562f40c7b2dfbb7398dc3d6fcc2d
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2023-12-10 23:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (31.63%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,35 +11,18 @@
  */
 class Solution {
 public:
+vector<int>ans;
+void inorder(TreeNode * root){
+    if(root == NULL ) return ;
+    // NLR 
+   
+    inorder(root->left);
+     ans.push_back(root->val);
+    inorder(root->right);
+}
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        TreeNode * curr = root;
-        while(curr){
-            // left node is  null , then visit and then go right
-            if(curr->left == nullptr){
-                ans.push_back(curr->val);
-                curr = curr->right;
-            }else{
-                // left is not null 
-                // find inorder predecessor 
-                TreeNode *pred = curr->left;
-                while(pred->right != curr && pred->right){
-                    pred = pred->right;
-                }
+        inorder(root);
+        return ans;
 
-                // if right node is null, then go left agter establishing link from pred to curr 
-                if(pred->right == nullptr){
-                    pred->right = curr;
-                    curr = curr->left;
-                }
-                else{
-                    // left is already visited , Go right after visitng curr node while removing the link
-                    pred -> right = nullptr;
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                }
-            }
-        }
-return ans;
     }
 };
