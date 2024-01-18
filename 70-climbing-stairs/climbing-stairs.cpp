df4cc4f456a759a8c71b9dd9e7af48594dd49144
Kshitij roodkee <76839614+horizenight@users.noreply.github.com>
2024-01-18 11:53:10 +0530

    Time: 3 ms (16.28%) | Memory: 6.8 MB (8.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,61 +1,14 @@
 class Solution {
 public:
-    int solveUsingRecursion(int n){
-        if(n == 0 || n == 1 ){
-            return 1;
-        }
-       
-        int ans = solveUsingRecursion(n-1) + solveUsingRecursion(n-2);
-
-        return ans;
+    int solve(int n , vector<int>&dp){
+         if(n ==0) return 1;
+        if(n < 0) return 0;
+        if(dp[n] != -1) return dp[n];
+       return dp[n] = solve(n-1,dp)+solve(n-2,dp) ;
     }
-    int solveUsingMem(int n , vector<int> &dp){
-           if(n == 0 || n == 1 ){
-            return 1;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-       
-        int ans = solveUsingMem(n-1,dp) + solveUsingMem(n-2,dp);
-
-        return dp[n] = ans;
-    }
-
-    int solveUsingTab(int n){
-        vector<int> dp(n+1,0);
-        // base case into dp 
-        dp[0] = 1;
-        dp[1] = 1;
-        // now dp array will go opposite from Mem
-        for(int i = 2 ; i <= n ;i++){
-            dp[i] = dp[i-1] + dp[i-2];
-            
-        }
-        return dp[n];
-    }
-
-    int solveUsingTabSO(int n){
-        if(n<=1){
-            return 1;
-        }
-        int prev1 = 1;
-        int prev2 = 1;
-        int ans ;
-        for(int i =2 ; i<=n ;i++){
-            ans = prev1 + prev2;
-            // shift 
-            prev2 = prev1;
-            prev1 = ans;
-        }
-
-        return ans;
-    }
-
+   
     int climbStairs(int n) {
-        // insitalising dp array 
-        vector<int> dp(n+1,-1);
-        int ans = solveUsingTabSO( n );
-        return ans;
+      vector<int>dp(n+1,-1);
+      return solve(n,dp);
     }
 };
